1b01662464d7a29de34a7adf47ab575ede17600f
santiagosalvatierra <83090531+santiagosalvatierra@users.noreply.github.com>
2021-12-14 17:31:07 -0800

    Repositories
    Agrego metodo en repository

--- ProyectoFinal/src/main/java/com/proyectofinal/repositorios/RepositorioComprobante.java
+++ ProyectoFinal/src/main/java/com/proyectofinal/repositorios/RepositorioComprobante.java
@@ -5,11 +5,16 @@
  */
 package com.proyectofinal.repositorios;
 
+import com.proyectofinal.entidades.Camion;
 import com.proyectofinal.entidades.Comprobante;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepositorioComprobante extends JpaRepository<Comprobante, String> {
-    
+    @Query("SELECT c FROM Comprobante c WHERE c.id = :id")
+    public List <Comprobante> buscarComprobanteporId(@Param("id")String id);
 }
--- ProyectoFinal/src/main/java/com/proyectofinal/repositorios/RepositorioViaje.java
+++ ProyectoFinal/src/main/java/com/proyectofinal/repositorios/RepositorioViaje.java
@@ -5,11 +5,17 @@
  */
 package com.proyectofinal.repositorios;
 
+import com.proyectofinal.entidades.Usuario;
 import com.proyectofinal.entidades.Viaje;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepositorioViaje extends JpaRepository <Viaje,String>{
+      @Query("SELECT c FROM Viaje c WHERE c.id = :id")
+    public List <Viaje> buscarViajeporId(@Param("id")String id);
     
 }
