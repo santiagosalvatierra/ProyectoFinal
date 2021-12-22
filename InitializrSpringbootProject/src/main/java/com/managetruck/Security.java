cd7ddfd1d003c2accfa77d896bfec81feb5fd886
santiagosalvatierra <83090531+santiagosalvatierra@users.noreply.github.com>
2021-12-21 16:39:27 -0800

    Security Areglo

--- InitializrSpringbootProject/src/main/java/com/managetruck/Security.java
+++ InitializrSpringbootProject/src/main/java/com/managetruck/Security.java
@@ -5,14 +5,13 @@
  */
 package com.managetruck;
 
-<<<<<<< Updated upstream
 import com.managetruck.servicios.ProveedorServicio;
 import com.managetruck.servicios.UsuarioServicio;
 import static javafx.scene.input.KeyCode.T;
-=======
+
 
 import com.managetruck.servicios.UsuarioServicio;
->>>>>>> Stashed changes
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -21,28 +20,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Security extends WebSecurityConfigurerAdapter {
     
-<<<<<<< Updated upstream
-    @Autowired
-    private UsuarioServicio usuario;
-    
-    
-    
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(usuario).passwordEncoder(new BCryptPasswordEncoder());
-=======
     @Autowired(required=true)
     private UsuarioServicio usuarioServicio;
     
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(usuarioServicio).passwordEncoder(new BCryptPasswordEncoder());
->>>>>>> Stashed changes
     }
 
     @Override
@@ -62,7 +50,3 @@ public class Security extends WebSecurityConfigurerAdapter {
     
     
 }
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
