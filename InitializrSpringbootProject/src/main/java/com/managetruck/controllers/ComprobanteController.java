2aa290527d9b67b535946518160abd2bb353d2f2
santiagosalvatierra <83090531+santiagosalvatierra@users.noreply.github.com>
2021-12-31 09:53:14 -0800

    try{}catch(){}

--- InitializrSpringbootProject/src/main/java/com/managetruck/controllers/ComprobanteController.java
+++ InitializrSpringbootProject/src/main/java/com/managetruck/controllers/ComprobanteController.java
@@ -1,4 +1,3 @@
-
 package com.managetruck.controllers;
 
 import com.managetruck.entidades.Comprobante;
@@ -11,6 +10,8 @@ import com.managetruck.repositorios.RepositorioViaje;
 import com.managetruck.servicios.ViajeServicio;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/comprobante")
 public class ComprobanteController {
+
     @Autowired
     private ViajeServicio viajeServicio;
     @Autowired
@@ -29,27 +31,31 @@ public class ComprobanteController {
     private RepositorioComprobante repositorioComprobante;
     @Autowired
     private RepositorioTransportista repositorioTransportista;
-    
+
     @GetMapping("/mostrarTranspostistas")//muestra los transportistas que aplicaron a un viaje en especifico
-    public String mostrarTranspostistas(Model model,String id_viaje) {
-        Optional <Viaje> viaje = repositorioViaje.findById(id_viaje);
-        List <Transportista> listadoTransportistas =viaje.get().getListadoTransportista();
-        model.addAttribute("tittle","Listado Transportistas");
-        model.addAttribute("listadoTransportistas",listadoTransportistas);
+    public String mostrarTranspostistas(Model model, String id_viaje) {
+        Optional<Viaje> viaje = repositorioViaje.findById(id_viaje);
+        List<Transportista> listadoTransportistas = viaje.get().getListadoTransportista();
+        model.addAttribute("tittle", "Listado Transportistas");
+        model.addAttribute("listadoTransportistas", listadoTransportistas);
         return null;
     }
-    
+
     @PostMapping("/votacion")//proveedor elije el transportista que va a ser responsable del viaje
-    public String votacion(String id_proveedor, String id_transportista, String id_viaje) throws ErroresServicio{
-        //buscamos el comprobante con el id del viaje
-        Optional <Comprobante> comprobante = repositorioComprobante.buscarComprobanteporIdViaje(id_viaje);
-        //comprueba que el id del proveedor sea igual al id del proveedor que creo el, comprobante
-        if (id_proveedor.equals(comprobante.get().getProveedor().getId())) {
-            Optional <Transportista> transportista = repositorioTransportista.findById(id_transportista);
-            transportista.get().getComprobante().add(comprobante);
-            //busca el transportista y le setea el comprobante
-        }else{
-            throw new ErroresServicio("Usted no es el proveedor que creo el viaje, no puede elejir el transportista");
+    public String votacion(String id_proveedor, String id_transportista, String id_viaje) throws ErroresServicio {
+        try {
+            //buscamos el comprobante con el id del viaje
+            Optional<Comprobante> comprobante = repositorioComprobante.buscarComprobanteporIdViaje(id_viaje);
+            //comprueba que el id del proveedor sea igual al id del proveedor que creo el, comprobante
+            if (id_proveedor.equals(comprobante.get().getProveedor().getId())) {
+                Optional<Transportista> transportista = repositorioTransportista.findById(id_transportista);
+                transportista.get().getComprobante().add(comprobante);
+                //busca el transportista y le setea el comprobante
+            } else {
+                throw new ErroresServicio("Usted no es el proveedor que creo el viaje, no puede elejir el transportista");
+            }
+        } catch (ErroresServicio ex) {
+            Logger.getLogger(ComprobanteController.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
