b4be67edab38c215bfd9c429e41166855e868ba9
Sergio <91640683+SergioRReal@users.noreply.github.com>
2021-12-31 20:51:54 -0300

    incorporar errores al metodo aplicar
    por si alguna de las condiciones no se cumplen y no ejecute el metodo

--- InitializrSpringbootProject/src/main/java/com/managetruck/servicios/ViajeServicio.java
+++ InitializrSpringbootProject/src/main/java/com/managetruck/servicios/ViajeServicio.java
@@ -1,6 +1,5 @@
 package com.managetruck.servicios;
 
-
 import com.managetruck.entidades.Foto;
 import com.managetruck.entidades.Proveedor;
 import com.managetruck.entidades.Transportista;
@@ -18,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ViajeServicio {
+
     @Autowired(required = true)
     NotificacionDeServicio notificacionServicio;
 
@@ -27,13 +27,13 @@ public class ViajeServicio {
     RepositorioTransportista repositorioTransportista;
     @Autowired
     ComprobanteServicio comprobanteServicio;
-    
+
     @Autowired
     RepositorioProveedor repositorioProveedor;
-    
+
     @Transactional
 
-    public void crearViaje(String idProveedor,Integer peso, Integer kmRecorridos, String tipoCargas, String destino, String origen) throws ErroresServicio {
+    public void crearViaje(String idProveedor, Integer peso, Integer kmRecorridos, String tipoCargas, String destino, String origen) throws ErroresServicio {
         ValidarViaje(peso, kmRecorridos, tipoCargas, destino, origen);
         Viaje viaje = new Viaje();
         viaje.setDestino(destino);
@@ -41,7 +41,7 @@ public class ViajeServicio {
         viaje.setOrigen(origen);
         viaje.setPeso(peso);
         viaje.setTipoCargas(tipoCargas);
-        Optional <Proveedor> proveedor = repositorioProveedor.findById(idProveedor);
+        Optional<Proveedor> proveedor = repositorioProveedor.findById(idProveedor);
         comprobanteServicio.crearComprobante(proveedor.get(), viaje);
         repositorioViaje.save(viaje);
     }
@@ -81,7 +81,6 @@ public class ViajeServicio {
         }
     }
 
-    
     @Transactional
     public void BajaViaje(String id) throws ErroresServicio {
         Optional<Viaje> respuesta = repositorioViaje.findById(id);
@@ -94,13 +93,21 @@ public class ViajeServicio {
             throw new ErroresServicio("No se encontro el viaje solicitado");
         }
     }
+
     @Transactional
-    public void aplicar(String id_transportista,String id_viaje){
+    public void aplicar(String id_transportista, String id_viaje) throws ErroresServicio{
         Optional<Viaje> viaje = repositorioViaje.findById(id_viaje);
-        Optional<Transportista> transportista = repositorioTransportista.findById(id_transportista);
-        viaje.get().getListadoTransportista().add(transportista.get());
-        notificacionServicio.enviar("TEXTO DE APLICACION A VIAJE", "NOMBRE DE LA PAGINA", transportista.get().getMail());
-           
+        if (viaje.isPresent()) {
+            Optional<Transportista> transportista = repositorioTransportista.findById(id_transportista);
+            if (transportista.isPresent()) {//si se cumplen la consiciones de que exita el viaje y el trasnportista
+                viaje.get().getListadoTransportista().add(transportista.get());
+                notificacionServicio.enviar("TEXTO DE APLICACION A VIAJE", "NOMBRE DE LA PAGINA", transportista.get().getMail());
+            }else{
+                throw new ErroresServicio("no se ha encontrado al trasnportista");
+            }   
+        }else{
+           throw new ErroresServicio("no se ha encontrado el viaje"); 
+        }
     }//metodo para que un transportista aplique a un viaje, lo agrega dentro de un array
     //luego el rpoveedor eligira entre todos los transportistas que haya en su viaje
 
