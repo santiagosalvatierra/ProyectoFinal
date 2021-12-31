b37c757b0b57990af95b1e4ee3ba3f68519026b4
santiagosalvatierra <83090531+santiagosalvatierra@users.noreply.github.com>
2021-12-31 09:57:56 -0800

    Try carch else .isPresent

--- InitializrSpringbootProject/src/main/java/com/managetruck/controllers/ComprobanteController.java
+++ InitializrSpringbootProject/src/main/java/com/managetruck/controllers/ComprobanteController.java
@@ -45,15 +45,27 @@ public class ComprobanteController {
     public String votacion(String id_proveedor, String id_transportista, String id_viaje) throws ErroresServicio {
         try {
             //buscamos el comprobante con el id del viaje
+
             Optional<Comprobante> comprobante = repositorioComprobante.buscarComprobanteporIdViaje(id_viaje);
             //comprueba que el id del proveedor sea igual al id del proveedor que creo el, comprobante
-            if (id_proveedor.equals(comprobante.get().getProveedor().getId())) {
-                Optional<Transportista> transportista = repositorioTransportista.findById(id_transportista);
-                transportista.get().getComprobante().add(comprobante);
-                //busca el transportista y le setea el comprobante
+            if (comprobante.isPresent()) {
+                if (id_proveedor.equals(comprobante.get().getProveedor().getId())) {
+                    Optional<Transportista> transportista = repositorioTransportista.findById(id_transportista);
+                    if(transportista.isPresent()){
+                    transportista.get().getComprobante().add(comprobante);
+                    }else{
+                         throw new ErroresServicio("El transportista no existe o no se pudo encontrar");
+               
+                    }
+                    //busca el transportista y le setea el comprobante
+                } else {
+                    throw new ErroresServicio("Usted no es el proveedor que creo el viaje, no puede elejir el transportista");
+                }
             } else {
-                throw new ErroresServicio("Usted no es el proveedor que creo el viaje, no puede elejir el transportista");
+                throw new ErroresServicio("El comprobante no existe o no se pudo encontrar");
+
             }
+
         } catch (ErroresServicio ex) {
             Logger.getLogger(ComprobanteController.class.getName()).log(Level.SEVERE, null, ex);
         }
