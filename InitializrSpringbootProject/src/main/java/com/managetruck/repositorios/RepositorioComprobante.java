3e2643f7c4f1d9fc79fd1a97a9a23216c3077804
santiagosalvatierra <83090531+santiagosalvatierra@users.noreply.github.com>
2021-12-31 08:31:58 -0800

    ajuste

--- InitializrSpringbootProject/src/main/java/com/managetruck/repositorios/RepositorioComprobante.java
+++ InitializrSpringbootProject/src/main/java/com/managetruck/repositorios/RepositorioComprobante.java
@@ -8,6 +8,7 @@ package com.managetruck.repositorios;
 
 import com.managetruck.entidades.Comprobante;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +18,7 @@ import org.springframework.stereotype.Repository;
 public interface RepositorioComprobante extends JpaRepository<Comprobante, String> {
     @Query("SELECT c FROM Comprobante c WHERE c.id = :id")
     public List <Comprobante> buscarComprobanteporId(@Param("id")String id);
+      
+    @Query("SELECT c FROM Comprobante c WHERE c.viaje.id LIKE :viaje_id")
+    public Optional <Comprobante> buscarComprobanteporIdViaje(@Param("viaje_id")String viaje_id);
 }
