9926e450bb4eedbba77122fa55e78ce4b2b9670c
agumey_ <57041890+agumey@users.noreply.github.com>
2022-01-06 01:30:19 -0300

    Ajustes vistas varias
    Incorporé Fragment para INDEX Empresa
    Ajustamos NAV del index general
    Comence IndexEmpresa
    Cree un CSS para IndexEmpresa
    Incorpore jQuery para Carrusel indexEmpresa en animacion.js pero no funciona. Tengo que resolverlo.

--- InitializrSpringbootProject/src/main/resources/templates/animacion.js
+++ InitializrSpringbootProject/src/main/resources/templates/animacion.js
@@ -16,4 +16,59 @@ var myInput = document.getElementById('myInput')
          
   myModal.addEventListener('shown.bs.modal', function () {
     myInput.focus()
-     })         
+     })        
+
+
+/* ANIMACIÓN CARRUSEL INDEX EMPRESA
+RESOLVER BIEN!
+
+<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+<script>
+var current = 0;
+var imagenes = new Array();
+ 
+$(document).ready(function() {
+    var numImages = 6;
+    if (numImages <= 3) {
+        $('.right-arrow').css('display', 'none');
+        $('.left-arrow').css('display', 'none');
+    }
+ 
+    $('.left-arrow').on('click',function() {
+        if (current > 0) {
+            current = current - 1;
+        } else {
+            current = numImages - 3;
+        }
+ 
+        $(".carrusel").animate({"left": -($('#product_'+current).position().left)}, 600);
+ 
+        return false;
+    });
+ 
+    $('.left-arrow').on('hover', function() {
+        $(this).css('opacity','0.5');
+    }, function() {
+        $(this).css('opacity','1');
+    });
+ 
+    $('.right-arrow').on('hover', function() {
+        $(this).css('opacity','0.5');
+    }, function() {
+        $(this).css('opacity','1');
+    });
+ 
+    $('.right-arrow').on('click', function() {
+        if (numImages > current + 3) {
+            current = current+1;
+        } else {
+            current = 0;
+        }
+ 
+        $(".carrusel").animate({"left": -($('#product_'+current).position().left)}, 600);
+ 
+        return false;
+    }); 
+ });
+</script>
+*/
