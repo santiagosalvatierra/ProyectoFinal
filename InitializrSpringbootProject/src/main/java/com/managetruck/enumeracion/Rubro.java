5ad93184cc6a16a1fbc796278988c661a348a43d
EmaOrtiz1 <95687906+EmaOrtiz1@users.noreply.github.com>
2022-01-24 21:26:00 -0300

    Enum.Rubro

---
+++ InitializrSpringbootProject/src/main/java/com/managetruck/enumeracion/Rubro.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.managetruck.enumeracion;
+
+/**
+ *
+ * @author emaor
+ */
+public enum Rubro {
+    Frigorifico, Alimenticion,Construccion,A_granel,Drogueria,Industrial;
+}
